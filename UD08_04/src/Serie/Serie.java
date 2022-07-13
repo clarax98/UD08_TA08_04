70410322a085edcd4dc447ce4a711eca70825bdb
Clara Ogalla <claraogalla@gmail.com>
2022-07-13 11:07:36 +0200

    V0.1

---
+++ UD08_04/src/Serie/Serie.java
@@ -0,0 +1,5 @@
+package Serie;
+
+public class Serie {
+
+}
---
+++ UD08_04/src/Serie/mainApp.java
@@ -0,0 +1,10 @@
+package Serie;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
